622c923b2cb6cb19dae87af5e8a9e04038cd519b
Jeff Burt <jburt1992@gmail.com>
2017-04-26 17:14:41 -0400

    add testing pods

---
+++ Example/Pods/Target Support Files/PinEntryView/PinEntryView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PinEntryView : NSObject
+@end
+@implementation PodsDummy_PinEntryView
+@end
---
+++ Example/Pods/Target Support Files/Pods-PinEntryView_Example/Pods-PinEntryView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PinEntryView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PinEntryView_Example
+@end
