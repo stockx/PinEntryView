baae24082d13dac9bd975f62251feb97aa488140
Jeff Burt <jburt1992@gmail.com>
2017-04-27 16:22:50 -0400

    add descriptions in code explaining what's going on

--- Example/PinEntryView/ViewController.swift
+++ Example/PinEntryView/ViewController.swift
@@ -11,6 +11,8 @@ import PinEntryView
 
 class ViewController: UIViewController {
     @IBOutlet fileprivate weak var pinEntryView: PinEntryView!
+    
+    // These are PinEntryView settings that can be configured
     @IBOutlet fileprivate weak var pinTextField: UITextField!
     @IBOutlet fileprivate weak var allowsBackspaceSwitch: UISwitch!
     @IBOutlet fileprivate weak var showsPlaceholderSwitch: UISwitch!
@@ -19,8 +21,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the delegate to get notified when the user finishes filling in
+        // every box with text.
         pinEntryView.delegate = self
         
+        // This state gets built from the defaults set in Interface Builder.
+        // If PinEntryView is created in code, you will want to create a new
+        // state using PinEntryView.State(...) and then set 
+        // pinEntryView.state = state
         let state = pinEntryView.state
         pinTextField.text = state?.pin?.uppercased()
         allowsBackspaceSwitch.isOn = state?.allowsBackspace == true
@@ -29,6 +37,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapSwitch(_ sender: UISwitch) {
+        // By creating a var, we group up the changes that way the internal 
+        // PinEntryView.update() method gets called just one time. Alternatively,
+        // you can set the vars directly on pinEntryView.state, causing the
+        // internal PinEntryView.update() to get called each time.
         var state = pinEntryView.state
         state?.allowsBackspace = allowsBackspaceSwitch.isOn
         state?.showsPlaceholder = showsPlaceholderSwitch.isOn
@@ -43,6 +55,8 @@ extension ViewController: PinEntryViewDelegate {
     }
 }
 
+// This extension is just for the Settings text field where you can adjust what
+// the accepted PIN is.
 extension ViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let newText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string).uppercased()
